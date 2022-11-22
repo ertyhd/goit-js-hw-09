16133dd841b335eb47dce9587cedaf4d1e0f3d0c
ertyhd <orionship@protonmail.com>
2022-11-22 19:02:11 +0200

    Ended first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,7 +3,7 @@ const refs = {
   startBtn: document.querySelector('[data-start]'),
   stopBtn: document.querySelector('[data-stop]'),
 };
-
+refs.stopBtn.setAttribute('disabled', true);
 const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
@@ -11,10 +11,14 @@ let timerId = null;
 
 refs.startBtn.addEventListener('click', () => {
   timerId = setInterval(() => {
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled');
     refs.bodyColor.style.backgroundColor = getRandomHexColor();
   }, 1000);
 });
 
 refs.stopBtn.addEventListener('click', () => {
+  refs.stopBtn.setAttribute('disabled', true);
+  refs.startBtn.removeAttribute('disabled');
   clearInterval(timerId);
 });
